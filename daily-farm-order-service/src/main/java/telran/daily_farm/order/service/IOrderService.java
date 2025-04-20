14be5e0f88d53ea15e7ff17c95f48c4de6f13017
vist2022 <vitalistudy@yahoo.com>
2025-04-20 21:52:24 +0300

    init commit

---
+++ daily-farm-order-service/src/main/java/telran/daily_farm/order/service/IOrderService.java
@@ -0,0 +1,15 @@
+package telran.daily_farm.order.service;
+
+import java.util.UUID;
+
+import telran.daily_farm.order.api.dto.CreateOrderResponseDto;
+import telran.daily_farm.order.entity.OrderFarmSet;
+
+public interface IOrderService {
+
+	CreateOrderResponseDto createOrder(UUID farmsetId, UUID customerId);
+
+	void checkPendingPayments();
+
+	void cancelOrder(OrderFarmSet order);
+}
